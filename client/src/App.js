dd46bc1322e5a55948505a53312e8becc73d07e2
thelandlorderwebsite-a11y <thelandlorderwebsite@gmail.com>
2026-04-08 12:48:05 -0700

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import AdminLogin from "./AdminLogin";
+import AIChat from "./AIChat";
+
+function App() {
+return (
+<div style={{ padding: 20 }}>
+<h1>Landlorder 🚀</h1>
+
+<AdminLogin />
+
+<hr />
+
+<AIChat />
+</div>
+);
+}
+
+export default App;
